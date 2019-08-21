10c926a638590f1bfad08da99649a3bc14f75ec3
amorist <amor90s.zhang@gmail.com>
2019-08-21 10:06:28 +0800

    remove dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function Renderer(baseTemplate: string, context: object, data: object, template: string): Promise<{}>;
-export declare function RendererTemplate(data: object, template: string): Promise<{}>;
